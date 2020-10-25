eb21ea1aeea813919c958ad5d14865e188743b14
FKD13 <francisklinck@gmail.com>
2020-10-25 10:45:11 +0100

    fix: suits have fixed colors

--- game/Card.py
+++ game/Card.py
@@ -10,16 +10,19 @@ class Color(enum.Enum):
 class Suit(enum.Enum):
     Clubs = 1
     Diamonds = 2
-    Hearts = 3
-    Spades = 4
+    Spades = 3
+    Hearts = 4
+
+    def get_color(self):
+        """Suits have fixed colors"""
+        return Color.Red if self.value % 2 == 0 else Color.Black
 
 
 class Card:
     """"A card in the deck"""
 
-    def __init__(self, value: int, color: Color, suit: Suit, name: str):
+    def __init__(self, value: int, suit: Suit, name: str):
         self.value = value
-        self.color = color
         self.suit = suit
         self.name = name
 
@@ -27,7 +30,7 @@ class Card:
         return self.value
 
     def get_color(self) -> Color:
-        return self.color
+        return self.suit.get_color()
 
     def get_suit(self) -> Suit:
         return self.suit
@@ -36,19 +39,21 @@ class Card:
         return self.name
 
     def __eq__(self, other):
-        return self.value == other.value and self.color == other.color
+        return self.value == other.value and self.suit.get_color() == other.suit.get_color()
 
     def __ne__(self, other):
         return not self == other
 
     def __lt__(self, other):
-        return self.value < other.value or (self.value == other.value and self.color < other.color)
+        return self.value < other.value or (
+                    self.value == other.value and self.suit.get_color() < other.suit.get_color())
 
     def __le__(self, other):
         return self < other or self == other
 
     def __gt__(self, other):
-        return self.value > other.value or (self.value == other.value and self.color > other.value)
+        return self.value > other.value or (
+                    self.value == other.value and self.suit.get_color() > other.suit.get_color())
 
     def __ge__(self, other):
         return self > other or self == other
@@ -59,8 +64,7 @@ def initial_deck_cards():
     values = [(3, "3"), (4, "4"), (5, "5"), (6, "6"), (7, "7"), (8, "8"), (9, "9"), (10, "10"), (11, "Jack"),
               (12, "Queen"), (13, "King"), (14, "Ace"), (15, "2")]
     for value in values:
-        for color in Color:
-            for suit in Suit:
-                cards.append(Card(value[0], color, suit, color.name + " " + values[1] + " of " + suit))
+        for suit in Suit:
+            cards.append(Card(value[0], suit, suit.get_color() + " " + values[1] + " of " + suit))
     random.shuffle(cards)
     return cards
