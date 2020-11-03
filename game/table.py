459ac6f579dd3b231faab76712aed4668cfd9329
FKD13 <francisklinck@gmail.com>
2020-11-03 17:30:13 +0100

    verify cards in players hand

--- game/table.py
+++ game/table.py
@@ -35,15 +35,29 @@ class Table:
         Take a move from an agent, execute the move on the table and give a reward to the agent.
         Validate if the move is valid first.
 
-        TODO: discuss this
+        TODO: discuss this.
+        TODO: move rewards to settings file.
         Reward scheme:
         - Invalid move: -10
         - else return game specific reward
 
         return the reward and if the move is final.
         """
-        # TODO: check if move is valid.
-        return self.game.on_move(agent, cards)[1:]
+        # A pass is a valid move.
+        if len(cards) != 0:
+            # WARNING: when playing with 2 decks of cards this is not sufficient.
+            if not all(card in agent.player.hand for card in cards):
+                return -10, False
+
+        # At this point we know the card is valid, pass the move to the game logic.
+        valid, reward, final = self.game.on_move(agent, cards)[1:]
+
+        # The move is valid, the cards can be moved from the players hand to the table.
+        if valid:
+            [agent.player.hand.remove(card) for card in cards]
+            self.played_cards.append((cards, agent))
+
+        return reward, final
 
     def last_move(self) -> Optional[Tuple[List[Card], Agent]]:
         return self.played_cards[-1] if len(self.played_cards) > 0 else None
