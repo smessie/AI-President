e368735dbd34135c24890ebf0b930ac7cfeed85e
smessie <smessie@smessie.com>
2020-11-10 20:20:22 +0100

    Implement a RandomAgent

---
+++ agents/random_agent.py
@@ -0,0 +1,29 @@
+import random
+
+from game.agent import Agent
+from game.table import Table
+from game.player import Player
+from typing import List, TYPE_CHECKING
+
+if TYPE_CHECKING:
+    from game.card import Card
+
+
+class RandomAgent(Agent):
+    def __init__(self):
+        super().__init__(Player())
+
+    def make_move(self, table: Table) -> None:
+        """
+        Let the agent make a random move from all possible moves in his state.
+        """
+        possible_moves: List[List[Card]] = self.player.get_all_possible_moves(table)
+        table.try_move(self, random.choice(possible_moves))
+
+    def get_preferred_card_order(self, table: Table) -> List[Card]:
+        """
+        Returns the preferred cards to exchange in the beginning of a round in random order.
+        """
+        possible_cards = list(set(table.deck.card_stack) - set(self.player.hand))
+        random.shuffle(possible_cards)
+        return possible_cards
