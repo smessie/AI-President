7706d1d0b368f6f17cffed9f5f56085f32635308
smessie <smessie@smessie.com>
2020-11-11 09:51:33 +0100

    Fix the RandomAgent and add it to the game

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 from game.president import President
 from agents.basic_agent import BasicAgent
 from agents.simple_agent import SimpleAgent
+from agents.random_agent import RandomAgent
 
 if __name__ == "__main__":
     game = President([
         BasicAgent(),
-        *(SimpleAgent() for _ in range(3))
+        *(SimpleAgent() for _ in range(3)),
+        RandomAgent()
     ])
 
     # Start the game
