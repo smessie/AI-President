e52caa1747cd14fcb9482ef8bec8f91a54537c8a
FKD13 <francisklinck@gmail.com>
2020-12-10 15:11:21 +0100

    add graphs

---
+++ plot_losses.py
@@ -0,0 +1,26 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+def plot(file):
+    try:
+        plt.style.use('ggplot')
+        data = np.loadtxt(f"{file}.csv", delimiter=',', skiprows=0)
+        fig, ax = plt.subplots(figsize=(16, 9))
+
+        x = np.arange(data.shape[0])
+        ax.plot(x, data[:, 0], label=f"Loss")
+        ax.legend()
+
+        plt.savefig(f"{file}.png", bbox_inches='tight')
+    except Exception as e:
+        print(e)
+
+
+filename = 'data/saves/%s/results/%s'    # without extension
+
+for i in range(17):
+    plot(filename % (i, 'training_loss'))
+
+plot(filename % (17, 'training_loss-0'))
+plot(filename % (17, 'training_loss-1'))
---
+++ plot_wins.py
@@ -0,0 +1,34 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+def plot(file):
+    try:
+        plt.style.use('ggplot')
+
+        data = np.loadtxt(f"{file}.csv", delimiter=',', skiprows=0)
+
+        fig, ax = plt.subplots(figsize=(16, 9))
+
+        x = data[:, 0]
+        for i in range(1, data.shape[1]):
+            ax.plot(x, data[:, i], label=f"Times {i} th place")
+
+        ax.legend()
+
+        plt.savefig(f"{file}.png", bbox_inches='tight')
+        plt.close()
+    except Exception as e:
+        print(e)
+
+
+filename = 'data/saves/%s/results/%s'    # without extension
+
+for i in range(17):
+    plot(filename % (i, 'wins-0'))
+    plot(filename % (i, 'wins-benchmark'))
+
+plot(filename % (17, 'wins-0'))
+plot(filename % (17, 'wins-1'))
+plot(filename % (17, 'wins-benchmark-0'))
+plot(filename % (17, 'wins-benchmark-1'))
