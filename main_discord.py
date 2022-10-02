ef0f493f91a39c1ee4095ac9c32390f9ecea5022
smessie <smessie@smessie.com>
2022-10-02 12:37:47 +0200

    Specify intents to be able to get the message contents

--- main_discord.py
+++ main_discord.py
@@ -2,9 +2,15 @@
 import asyncio
 from threading import Thread
 
+import discord
+
 from discordbot.discord_bot import DiscordBot
 
 if __name__ == "__main__":
+    intents = discord.Intents.default()
+    intents.members = True
+    intents.messages = True
+
     discord_bot = DiscordBot()
     loop = asyncio.get_event_loop()
     loop.create_task(discord_bot.start(input('Enter bot token: ')))
